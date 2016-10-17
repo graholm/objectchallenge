e59861331bae2b8d692080fe50c4ad9e8262ec29
graholm <graholm@nuevaschool.org>
2016-10-17 09:22:17 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ var person = {
 drawperson()
 function drawperson() {  
 document.write(person.name)
-console.log("name")
 var img = document.createElement("img");
   img.src = (person.representation);
   $(img).css({"border-color": person.favoriteColor, 
               "border-style":"solid"});
   $(img).css ({height: person.height})
+  
   document.body.appendChild(img);
   
 }
