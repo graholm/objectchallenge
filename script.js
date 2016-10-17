934ac7ecb38a1bfa4aec68678b379ef5c82f26b4
graholm <graholm@nuevaschool.org>
2016-10-17 08:58:08 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var person = {
   name: "Grace",
-  representation: "http://i.dailymail.co.uk/i/pix/2016/03/14/19/322C3FA100000578-0-image-a-13_1457982438669.jpg",
+  representation: "http://www.menucool.com/slider/jsImgSlider/images/image-slider-2.jpg",
   height: "500px",	
   favoriteColor: "blue",
   catchphrase: "Get pumped!"
@@ -10,7 +10,7 @@ drawperson()
 function drawperson() {  
 document.write(person.name)
 console.log("name")
-document.write("img src="+this.link+">");
+var img = document.createElement (person.representation)
 
 
 }
