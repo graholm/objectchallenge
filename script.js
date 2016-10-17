5a1aeef9070323d0d17529b2fde59afb16546d86
graholm <graholm@nuevaschool.org>
2016-10-17 09:17:46 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var person = {
   name: "Grace",
   representation: "http://www.menucool.com/slider/jsImgSlider/images/image-slider-2.jpg",
   height: "100px",	
-  favoriteColor: "blue",
+  favoriteColor: "pink",
   catchphrase: "Get pumped!"
 }
 
@@ -12,10 +12,9 @@ document.write(person.name)
 console.log("name")
 var img = document.createElement("img");
   img.src = (person.representation);
-  $(img).css({"border-color": "#C1E0FF", 
+  $(img).css({"border-color": person.favoriteColor, 
               "border-weight":"1px", 
               "border-style":"solid"});
-    
             ({height: person.height})
   document.body.appendChild(img);
   
